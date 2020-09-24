f7802a90b908727650cd20e45ffa5a10b3a97126
Paulius Staišiūnas <paulius.staisiunas@visma.com>
2020-09-25 00:31:50 +0300

    Initial project structure.

---
+++ src/main/java/com/sheet/twistorspace/SheetTwistorSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.sheet.twistorspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetTwistorSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheetTwistorSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sheet/twistorspace/SheetTwistorSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheet.twistorspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheetTwistorSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
